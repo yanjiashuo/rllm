a00a3242915d1a01c287dbfa9511fafc9675dd64
yanjiashuo <59167722+yanjiashuo@users.noreply.github.com>
2024-08-07 12:12:20 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='your_project_name',
+    version='0.1',
+    packages=['your_package_name'],
+    install_requires=[
+        # 在这里添加项目依赖
+    ],
+)
